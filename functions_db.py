15d395f7c3dcb6ce6f70666fded99415b0d7a31b
Fredecontre <fredecontre@gmail.com>
2023-01-12 17:36:06 +0100

    Création base de donnée (commit) update 5

--- functions_db.py
+++ functions_db.py
@@ -28,12 +28,20 @@ def get_text(text_id):
     ligne = c.fetchone()
     return ligne[0]
 
+def get_texts_user(username,password):
+    c.execute("SELECT * FROM Utilisateur_possede_texte")
+    table=c.fetchall()
+    #texts = [[table[i][0],table[i][1]] for i in range(len(table))]
+    texts = [get_text(table[i][1]) for i in range(len(table))]
+    return texts
+
+
 def get_texts():
     c.execute("SELECT contenu FROM Texte")
     ligne = c.fetchall()
     return [ligne[i][0] for i in range(len(ligne))]
 
-def add_text_public(text_content,is_private):
+def add_text(text_content,is_private):
     c.execute("INSERT INTO Texte (contenu,est_privee) VALUES (?,?) ;",[text_content,is_private])
     conn.commit()
 
@@ -44,7 +52,7 @@ def add_text_public(text_content,is_private):
   
 
 def add_text_private(text_content,username,password):
-    add_text_public(text_content,True)
+    id_text = add_text(text_content,True)
     c.execute("SELECT id FROM Texte ORDER BY id DESC LIMIT 1")
     id_text = c.fetchone()[0]
     c.execute("SELECT id FROM Utilisateur WHERE identifiant = (?) AND mot_de_passe = (?)", [username,password])
@@ -52,3 +60,4 @@ def add_text_private(text_content,username,password):
     c.execute("INSERT INTO Utilisateur_possede_texte VALUES (?,?)", [id_user,id_text])
     conn.commit()
 
+    return id_text
