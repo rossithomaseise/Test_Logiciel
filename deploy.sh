fe17bd34b30ce7c18a8cd975ddaf3007a5d098d5
rossithomaseise <rossi.thomas83140@gmail.com>
2023-01-21 19:10:03 +0100

    correction31 fichier wheel_docker.yml

--- deploy.sh
+++ deploy.sh
@@ -1,2 +1,3 @@
 docker pull ghcr.io/rossithomaseise/test-logiciel:devops-docker
+docker stop $(docker ps | grep ghcr.io/rossithomaseise/test-logiciel:devops-docker | awk '{print $1}' | head -n 1)
 docker run -d -p 8882:8882 ghcr.io/rossithomaseise/test-logiciel:devops-docker
